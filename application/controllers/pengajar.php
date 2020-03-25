ff30589cabc646930b30d0bf375d17acd7fa37a2
faizazharr <faizazharr@gmail.com>
2020-03-25 15:16:20 +0700

    update config

--- application/controllers/pengajar.php
+++ application/controllers/pengajar.php
@@ -10,7 +10,7 @@
 
             $this->load->view('part/header');
             $this->load->view('part/sidebarpengajar',$data);
-            $this->load->view('siswa/dashboard');
+            $this->load->view('pengajar/dashboard');
             $this->load->view('part/footer');
         }
         public function profile()
@@ -18,8 +18,61 @@
             $data['nama'] = $this->session->userdata('nama');
             
             $this->load->view('part/header');
-            $this->load->view('part/sidebarsiswa',$data);
-            $this->load->view('siswa/profile');
+            $this->load->view('part/sidebarpengajar',$data);
+            $this->load->view('pengajar/profile');
+            $this->load->view('part/footer');
+        }
+        public function Pesan()
+        {
+            $data['nama'] = $this->session->userdata('nama');
+            $this->load->view('part/header');
+            $this->load->view('part/sidebarpengajar',$data);
+            $this->load->view('pengajar/profile');
+            $this->load->view('part/footer');
+        }
+
+        public function jadwalMapel()
+        {
+            $data['nama'] = $this->session->userdata('nama');
+            $this->load->view('part/header');
+            $this->load->view('part/sidebarpengajar',$data);
+            $this->load->view('pengajar/profile');
+            $this->load->view('part/footer');
+        }
+
+        public function tugas()
+        {
+            $data['nama'] = $this->session->userdata('nama');
+            $this->load->view('part/header');
+            $this->load->view('part/sidebarpengajar',$data);
+            $this->load->view('pengajar/profile');
+            $this->load->view('part/footer');
+        }
+
+        public function materi()
+        {
+            $data['nama'] = $this->session->userdata('nama');
+            $this->load->view('part/header');
+            $this->load->view('part/sidebarpengajar',$data);
+            $this->load->view('pengajar/profile');
+            $this->load->view('part/footer');
+        }
+
+        public function filterPengajar()
+        {
+            $data['nama'] = $this->session->userdata('nama');
+            $this->load->view('part/header');
+            $this->load->view('part/sidebarpengajar',$data);
+            $this->load->view('pengajar/profile');
+            $this->load->view('part/footer');
+        }
+
+        public function filterSiswa()
+        {
+            $data['nama'] = $this->session->userdata('nama');
+            $this->load->view('part/header');
+            $this->load->view('part/sidebarpengajar',$data);
+            $this->load->view('pengajar/profile');
             $this->load->view('part/footer');
         }
     }
